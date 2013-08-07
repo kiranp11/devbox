85e37ecf465d61c1a9d4e704a8ace6404032073a
Pramod N <npramod05@gmail.com>
2013-08-07 14:57:26 +0530

    K1/Pramod hadoop install from tarball

--- puppet/modules/platform/manifests/init.pp
+++ puppet/modules/platform/manifests/init.pp
@@ -1,21 +1,43 @@
 class platform {
 
-   package{ "openjdk-6-jdk":
-     ensure => present;
-   }
+    package{ "openjdk-6-jdk":
+        ensure => present;
+    }
 
-   file {"/tmp/hadoop.deb":
-      source => "puppet:///modules/platform/hadoop_1.0.3-1_x86_64.deb"
-   }
+    file {"/etc/profile":
+        source => "puppet:///modules/platform/profile"
+    }
 
-   file {"/etc/profile":
-      source => "puppet:///modules/platform/profile"
-   }
+    exec { "get-hadoop-tar" :
+        command   => "wget http://archive.apache.org/dist//hadoop/core/hadoop-1.0.3/hadoop-1.0.3-bin.tar.gz -O /tmp/hadoop-1.0.3-bin.tar.gz",
+        logoutput => true,
+        unless    => "test -f /tmp/hadoop-1.0.3-bin.tar.gz",
+        timeout   => 0
+    }
 
-   exec {"dpkg -i /tmp/hadoop.deb":
-     logoutput => true,
-     require => File["/tmp/hadoop.deb"]
-   }
+    exec { "untar-hadoop" :
+        command   => "tar -xvf /tmp/hadoop-1.0.3-bin.tar.gz",
+        logoutput => true,
+        cwd       => "/usr/local",
+        require   => Exec["get-hadoop-tar"]
+    }
+
+    file {"/usr/local/hadoop-1.0.3/conf/core-site.xml":
+        source => "puppet:///modules/platform/core-site.xml",
+        require   => Exec["untar-hadoop"]
+    }
+
+    file {"/usr/local/hadoop-1.0.3/conf/mapred-site.xml":
+        source => "puppet:///modules/platform/mapred-site.xml",
+        require   => Exec["untar-hadoop"]
+    }
+
+    file {"/usr/local/hadoop-1.0.3/conf/hdfs-site.xml":
+        source => "puppet:///modules/platform/hdfs-site.xml",
+        require   => Exec["untar-hadoop"]
+    }
+
+
 
 }
 
